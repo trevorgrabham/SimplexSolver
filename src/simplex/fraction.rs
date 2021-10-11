a92f440fbeb327c937b7147e293d361a63b5ae10
tgrabham <tgrabham@sfu.ca>
2021-10-11 01:26:49 -0700

    Added a check for infinitely large numerators when doing comparisons, need to update it for addition, subtraction, etc.

--- src/simplex/fraction.rs
+++ src/simplex/fraction.rs
@@ -21,6 +21,7 @@ pub struct Fraction {
 
 impl Fraction {
     pub fn new(n: i64, d: i64) -> Fraction {
+        assert_ne!(d,0);
         Fraction {
             numerator: n,
             denominator: d,
@@ -36,29 +37,73 @@ impl Fraction {
 
 impl std::fmt::Display for Fraction {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}/{}", self.numerator, self.denominator)
-    }
-}
-
-impl Mul for Fraction {
-    fn mul(self, other: Fraction) -> Fraction {
-        Fraction {
-            let mut res = Fraction {
-                numerator: self.numerator * other.numerator,
-                denominator: self.denominator * other.denominator,
-            }
-            res.reduce()
+        if self.denominator == 1 {
+            write!(f, "{}", self.numerator)
+        } else {
+            write!(f, "{}/{}", self.numerator, self.denominator)
         }
     }
 }
 
-impl Div for Fraction {
+impl std::ops::Sub for Fraction {
+    type Output = Fraction;
+
+    fn sub(self, other: Fraction) -> Fraction {
+        let mut res = Fraction {
+            numerator: self.numerator*other.denominator - other.numerator*self.denominator,
+            denominator: self.denominator*other.denominator,
+        };
+        res.reduce();
+        res
+    }
+}
+
+impl std::ops::Add for Fraction {
+    type Output = Fraction;
+
+    fn add(self, other: Fraction) -> Fraction {
+        let mut res = Fraction {
+            numerator: self.numerator*other.denominator + other.numerator*self.denominator,
+            denominator: self.denominator*other.denominator,
+        };
+        res.reduce();
+        res
+    }
+}
+
+impl std::ops::Mul for Fraction {
+    type Output = Fraction;
+
+    fn mul(self, other: Fraction) -> Fraction {
+        let mut res = Fraction {
+        numerator: self.numerator * other.numerator,
+        denominator: self.denominator * other.denominator,
+        };
+        res.reduce();
+        res
+    }
+}
+
+impl std::ops::Div for Fraction {
+    type Output = Fraction;
+
     fn div(self, other: Fraction) -> Fraction {
             let mut res = Fraction {
                 numerator: self.numerator * other.denominator,
                 denominator: self.denominator * other.numerator,
-            }
-            res.reduce()
+            };
+            res.reduce();
+            res
+    }
+}
+
+impl PartialOrd for Fraction {
+    fn partial_cmp(&self, other: &Fraction) -> Option<std::cmp::Ordering> {
+        if self.numerator == i64::MAX || self.numerator == -i64::MAX || other.numerator == i64::MAX || other.numerator == -i64::MAX {
+            Some(self.numerator.cmp(&other.numerator))
+        } else {
+            Some((self.numerator*other.denominator).cmp(&(other.numerator*self.denominator)))
+        }
     }
 }
 
@@ -73,16 +118,18 @@ impl From<i64> for Fraction {
 
 impl From<f64> for Fraction {
     fn from(f: f64) -> Fraction {
-        let mut power: usize = 0;
-        while(f != math::round::floor(f)){
-            f *= 10;
-            power++;
+        let mut power: u32 = 0;
+        let mut n: f64 = f;
+        while n != n.floor() {
+            n *= 10f64;
+            power += 1;
         }
         let mut res = Fraction {
-            numerator: f as i64,
+            numerator: n as i64,
             denominator: 10i32.pow(power) as i64,
-        }
-        res.reduce()
+        };
+        res.reduce();
+        res
     }
 }
 
