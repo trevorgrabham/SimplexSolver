a63c008fdaf2a97d1a4c9e8bfe7712937478c970
trevorgrabham <tgrabham@sfu.ca>
2021-10-24 20:43:15 -0700

    Added submodules to organize the code. Moved some of the code out of the main body and into the submodules as functions. Next thing to do will be to make the tableau an object.

--- src/simplex/solver.rs
+++ src/simplex/solver.rs
@@ -1,5 +1,7 @@
 use super::fraction::Fraction;
 
+const DEBUG: bool = true;
+
 
 pub fn solve(A: Vec<Vec<f64>>, b: Vec<f64>, c: Vec<f64>) -> (Vec<Vec<Fraction>>, Vec<Fraction>, Vec<Fraction>, Fraction) {
     // Checks to make sure that the dimensions of our matrices are valid.
@@ -11,7 +13,7 @@ pub fn solve(A: Vec<Vec<f64>>, b: Vec<f64>, c: Vec<f64>) -> (Vec<Vec<Fraction>>,
     let n: usize = A.len();
 
     // scan to find the rows corresponding to I
-    let B: Vec<usize> = find_basis(&A);
+    let B: Vec<usize> = setup::find_basis(&A);
 
     for i in 0..m {
         assert_ne!(B[i], n+1, "We could not find a column in A corresponding to the {}th row of an identity matrix", i);
@@ -20,11 +22,11 @@ pub fn solve(A: Vec<Vec<f64>>, b: Vec<f64>, c: Vec<f64>) -> (Vec<Vec<Fraction>>,
     // The indecies corresponding to the identity matrix are stored in order
 
     // Computing the cost vector corresponding to our basis matrix B
-    let cB = get_cost_basis(&c, &B);
+    let cB = setup::get_cost_basis(&c, &B);
 
     // set up our reduced_cost vector and our objective value.
     // these are updated using our cost basis vector, and doing matrix multiplaction.
-    let (reduced_cost, obj) = compute_reduced_cost(&cB, &A, &b, &c);
+    let (reduced_cost, obj) = setup::compute_reduced_cost(&cB, &A, &b, &c);
 
     // set up our variables to be sent off to the iterative stage. 
     // made clones so that we can hand over ownership to the iterate function.
@@ -62,7 +64,6 @@ pub fn solve(A: Vec<Vec<f64>>, b: Vec<f64>, c: Vec<f64>) -> (Vec<Vec<Fraction>>,
 }
 
 fn iterate(A: Vec<Vec<Fraction>>, b: Vec<Fraction>, reduced_cost: Vec<Fraction>, obj: Fraction) -> Option<(Vec<Vec<Fraction>>, Vec<Fraction>, Vec<Fraction>, Fraction)> {
-    let DEBUG = true;
     // do not need to do bounds checking here, as it is already done in the function that calls this one
     let m = A[0].len();
     let n = A.len();
@@ -88,35 +89,23 @@ fn iterate(A: Vec<Vec<Fraction>>, b: Vec<Fraction>, reduced_cost: Vec<Fraction>,
     }
     // First we find our variable with a negative reduced cost
     // We do this using Bland's rule, so that there will not be cycling
-    let mut first_negative_reduced_cost: usize = n;
-    for i in 0..n {
-        if reduced_cost[i] < Fraction::from(0i64) {
-            first_negative_reduced_cost = i;
-            break;
-        }
-    }
-    if first_negative_reduced_cost == n {
+    let reduced_cost_index = solvers::bland_reduced_cost(&reduced_cost);
+
+    if reduced_cost_index == n {
         println!("Optimal solution found!");
         return None;
     }
     if DEBUG {
-        println!("Reduced cost index: {:?}", first_negative_reduced_cost);
+        println!("Reduced cost index: {:?}", reduced_cost_index+1);
     }
 
     // We now have to find our minimum ratio after selecting our entering variable
-    // In the case of a tie, we use Bland's rule and select the first in the sequence
-    let mut first_minimum_ratio = n;
-    let mut minimum_ratio = Fraction::from(i64::MAX);
-    for i in 0..m {
-        if A[first_negative_reduced_cost][i] > Fraction::from(0f64) && b[i].clone()/A[first_negative_reduced_cost][i].clone() < minimum_ratio {
-            minimum_ratio = b[i].clone()/A[first_negative_reduced_cost][i].clone();
-            first_minimum_ratio = i;
-        }
-    }
+    let (minimum_ratio, minimum_ratio_index) = solvers::find_min_ratio(&A, &b, &reduced_cost_index);
+
     if DEBUG {
-        println!("Minimum ratio: {}\tIndex: {:?}", minimum_ratio, first_minimum_ratio);
+        println!("Minimum ratio: {}\tIndex: {:?}", minimum_ratio, minimum_ratio_index);
     }
-    if minimum_ratio == Fraction::from(f64::INFINITY) {
+    if minimum_ratio == Fraction::from(i64::MAX) {
         println!("Linear program is unbounded. The resulting objective function value may or may not be optimal.");
         return None;
     }
@@ -132,81 +121,115 @@ fn iterate(A: Vec<Vec<Fraction>>, b: Vec<Fraction>, reduced_cost: Vec<Fraction>,
     // update our A matrix
     for col in 0..n {
         for row in 0..m {
-            if row == first_minimum_ratio {
-                new_A[col].push(A[col][row].clone()/A[first_negative_reduced_cost][row].clone());
+            if row == minimum_ratio_index {
+                new_A[col].push(A[col][row].clone()/A[reduced_cost_index][row].clone());
             } else {
-                new_A[col].push(A[col][row].clone() - (A[first_negative_reduced_cost][row].clone() * A[col][first_minimum_ratio].clone() / A[first_negative_reduced_cost][first_minimum_ratio].clone()));
+                new_A[col].push(A[col][row].clone() - (A[reduced_cost_index][row].clone() * A[col][minimum_ratio_index].clone() / A[reduced_cost_index][minimum_ratio_index].clone()));
             }
         }
     }
     // update our b matrix
     for i in 0..m {
-        if i == first_minimum_ratio {
-            new_b.push(b[i].clone()/A[first_negative_reduced_cost][i].clone());
+        if i == minimum_ratio_index {
+            new_b.push(b[i].clone()/A[reduced_cost_index][i].clone());
         } else {
-            new_b.push(b[i].clone() - (b[first_minimum_ratio].clone() * A[first_negative_reduced_cost][i].clone() / A[first_negative_reduced_cost][first_minimum_ratio].clone()));
+            new_b.push(b[i].clone() - (b[minimum_ratio_index].clone() * A[reduced_cost_index][i].clone() / A[reduced_cost_index][minimum_ratio_index].clone()));
         }
     }
     // update our reduced cost matrix
     for i in 0..n {
-        new_reduced_cost.push(reduced_cost[i].clone() - (reduced_cost[first_negative_reduced_cost].clone() * A[i][first_minimum_ratio].clone() / A[first_negative_reduced_cost][first_minimum_ratio].clone()));
+        new_reduced_cost.push(reduced_cost[i].clone() - (reduced_cost[reduced_cost_index].clone() * A[i][minimum_ratio_index].clone() / A[reduced_cost_index][minimum_ratio_index].clone()));
     }
     // update our objective value function
-    new_obj = obj - (b[first_minimum_ratio].clone() * reduced_cost[first_negative_reduced_cost].clone() / A[first_negative_reduced_cost][first_minimum_ratio].clone());
+    new_obj = obj - (b[minimum_ratio_index].clone() * reduced_cost[reduced_cost_index].clone() / A[reduced_cost_index][minimum_ratio_index].clone());
 
     Some((new_A, new_b, new_reduced_cost, new_obj))
 }
 
-// scans the columns of A, finding the identity matrix columns within. 
-// B[i] = n+1, when it is unable to find the i+1th column of the Identity matrix
-fn find_basis(A: &Vec<Vec<f64>>) -> Vec<usize> {
-    let m = A[0].len();
-    let n = A.len();
-    let mut B: Vec<usize> = vec![n+1 as usize;m];
-    let mut I = vec![0f64;m];
-    I[0] = 1f64;
+mod setup {
+    use super::Fraction;
+    // scans the columns of A, finding the identity matrix columns within. 
+    // B[i] = n+1, when it is unable to find the i+1th column of the Identity matrix
+    pub fn find_basis(A: &Vec<Vec<f64>>) -> Vec<usize> {
+        let m = A[0].len();
+        let n = A.len();
+        let mut B: Vec<usize> = vec![n+1 as usize;m];
+        let mut I = vec![0f64;m];
+        I[0] = 1f64;
 
-    for i in 0..n {
-        for j in 0..m {
-            if I == A[i] {
-                B[j] = i;
-            }
-            I.rotate_right(1);
-        }
-    }
-
-    B
-}
-
-fn get_cost_basis(c: &Vec<f64>, B: &Vec<usize>) -> Vec<Fraction> {
-    let m = B.len();
-    let mut cB = Vec::with_capacity(m);
-    for i in 0..m {
-        cB.push(Fraction::from(c[B[i]]));
-    }
-    cB
-}
-
-fn compute_reduced_cost(cB: &Vec<Fraction>, A: &Vec<Vec<f64>>, b: &Vec<f64>, c: &Vec<f64>) -> (Vec<Fraction>, Fraction) {
-    let n = A.len();
-    let m = A[0].len();
-    let mut reduced_cost = Vec::with_capacity(n);
-    let mut obj: Fraction = Fraction::from(0);
-    if *cB != vec![Fraction::from(0i64);m] {
         for i in 0..n {
-            let mut sum = Fraction::from(0);
             for j in 0..m {
-                sum = sum + Fraction::from(A[i][j]) * cB[j].clone();
+                if I == A[i] {
+                    B[j] = i;
+                }
+                I.rotate_right(1);
             }
-            reduced_cost.push(sum-Fraction::from(c[i]));
         }
-        for i in 0..m {
-            obj = obj + cB[i].clone() * Fraction::from(b[i]);
-        }
-    } else {
-        for i in 0..n {
-            reduced_cost.push(Fraction::from(-c[i]));
-        }
+
+        B
     }
-    (reduced_cost, obj)
+
+    pub fn get_cost_basis(c: &Vec<f64>, B: &Vec<usize>) -> Vec<Fraction> {
+        let m = B.len();
+        let mut cB = Vec::with_capacity(m);
+        for i in 0..m {
+            cB.push(Fraction::from(c[B[i]]));
+        }
+        cB
+    }
+
+    pub fn compute_reduced_cost(cB: &Vec<Fraction>, A: &Vec<Vec<f64>>, b: &Vec<f64>, c: &Vec<f64>) -> (Vec<Fraction>, Fraction) {
+        let n = A.len();
+        let m = A[0].len();
+        let mut reduced_cost = Vec::with_capacity(n);
+        let mut obj: Fraction = Fraction::from(0);
+        if *cB != vec![Fraction::from(0i64);m] {
+            for i in 0..n {
+                let mut sum = Fraction::from(0);
+                for j in 0..m {
+                    sum = sum + Fraction::from(A[i][j]) * cB[j].clone();
+                }
+                reduced_cost.push(sum-Fraction::from(c[i]));
+            }
+            for i in 0..m {
+                obj = obj + cB[i].clone() * Fraction::from(b[i]);
+            }
+        } else {
+            for i in 0..n {
+                reduced_cost.push(Fraction::from(-c[i]));
+            }
+        }
+        (reduced_cost, obj)
+    }
+}
+
+mod solvers {
+    use super::Fraction;
+    pub fn bland_reduced_cost(reduced_cost: &Vec<Fraction>) -> usize {
+        let n = reduced_cost.len();
+        for i in 0..n {
+            if reduced_cost[i] < Fraction::from(0i64) {
+                return i;
+            }
+        }
+        n
+    }
+
+    pub fn find_min_ratio(A: &Vec<Vec<Fraction>>, b: &Vec<Fraction>, reduced_cost_index: &usize) -> (Fraction, usize) {
+        let n = A.len();
+        let m = A[0].len();
+
+        let mut minimum_ratio_index = n;
+        let mut minimum_ratio = Fraction::from(i64::MAX);
+        for i in 0..m {
+            if A[*reduced_cost_index][i] <= Fraction::from(0) {
+                continue;
+            }else if b[i].clone()/A[*reduced_cost_index][i].clone() < minimum_ratio {
+                minimum_ratio = b[i].clone()/A[*reduced_cost_index][i].clone();
+                minimum_ratio_index = i;
+            }
+        }
+        (minimum_ratio, minimum_ratio_index)
+    }
+
 }
