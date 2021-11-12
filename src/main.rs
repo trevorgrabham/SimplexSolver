aad2832d722a0c1fa9139b6b59bf078adfbe05c9
tgrabham <tgrabham@sfu.ca>
2021-11-12 12:47:04 -0800

    testing to see if I set up my personal access token properly

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+// test test test
 mod simplex;
 use simplex::tableau::Tableau;
 
 fn main() {
-    let A = vec![vec![1f64,1f64,0f64], vec![2f64,1f64,1f64], vec![1f64,0f64,1f64], vec![0f64,2f64,-1f64], vec![1f64,0f64,0f64], vec![0f64,1f64,0f64], vec![0f64,0f64,1f64]]; 
-    let b = vec![50f64,30f64,20f64];
-    let c = vec![4f64,-1f64,0f64,2f64,-1000f64,-1000f64,-1000f64];
-    let mut tableau = Tableau::new(&A,&b,&c,String::from("standard"));
+    let A = vec![vec![0.25f64,0.5f64,0f64], vec![-8f64,-0.5f64,0f64], vec![-1f64,-0.5f64,1f64], vec![9f64,3f64,0f64], vec![1f64,0f64,0f64], vec![0f64,1f64,0f64], vec![0f64,0f64,1f64]]; 
+    let b = vec![0f64,0f64,1f64];
+    let c = vec![0.75f64,-20f64,0.5f64,-6f64,0f64,0f64,0f64];
+    let mut tableau = Tableau::new(&A,&b,&c,String::from("standard"),String::from("bland"),String::from("twophase"));
     tableau.set_debug(true);
     tableau.solve();
+    tableau.find_b_inverse();
 }
 
